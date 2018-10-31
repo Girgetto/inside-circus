32dc3879cc2c95a46aa6a30c67959a7d1b823885
Giorgio Grassini <giorgiograssini@gmail.com>
2018-10-31 16:57:56 +0100

    quotes responsive arrow to balls

--- javascript/quotes.js
+++ javascript/quotes.js
@@ -2,23 +2,35 @@ const buttonLeft = document.getElementById('arrow-left');
 const buttonRight = document.getElementById('arrow-right');
 const div1 = document.getElementById('quote1');
 const div2 = document.getElementById('quote2');
+const imgA = document.getElementById('img-alberto');
+const imgE = document.getElementById('img-emilio');
+const ball1 = document.getElementById('ball1');
+const ball2 = document.getElementById('ball2');
 
 buttonLeft.addEventListener('click', () => {
-  if (div1.classList[1] === 'active') {
-    div1.classList.toggle('active');
-    div2.classList.toggle('active');
-  } else {
-    div1.classList.toggle('active');
-    div2.classList.toggle('active');
-  }
+  div1.classList.toggle('active');
+  div2.classList.toggle('active');
 });
 
 buttonRight.addEventListener('click', () => {
-  if (div1.classList[1] === 'active') {
-    div1.classList.toggle('active');
-    div2.classList.toggle('active');
-  } else {
-    div1.classList.toggle('active');
-    div2.classList.toggle('active');
-  }
+  div1.classList.toggle('active');
+  div2.classList.toggle('active');
+});
+
+imgA.addEventListener('click', () => {
+  div1.classList.toggle('active');
+  div2.classList.toggle('active');
+  ball1.classList.remove('far');
+  ball1.classList.add('fas');
+  ball2.classList.remove('fas');
+  ball2.classList.add('far');
+});
+
+imgE.addEventListener('click', () => {
+  div1.classList.toggle('active');
+  div2.classList.toggle('active');
+  ball1.classList.remove('fas');
+  ball1.classList.add('far');
+  ball2.classList.remove('far');
+  ball2.classList.add('fas');
 });
